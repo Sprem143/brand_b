57a6521e4f13573b176ae05044cbfa58b0638218
Prem Kumar <prem68265@gmail.com>
2024-12-06 14:02:33 +0530

    Update inventory6.js

--- controller/Inventory_controller/inventory6.js
+++ controller/Inventory_controller/inventory6.js
@@ -90,8 +90,7 @@ exports.autofetchdata6 = async(req, res) => {
             if (utagData.sku_inventory.length == 1 && utagData.sku_inventory[0] === '0') {
                 return res.status(200).send(true);
             }
-    
-            if (utagData.sku_inventory.length > 1) {
+else{
                 saveData(utagData);
                  res.status(200).send(true);
             }
