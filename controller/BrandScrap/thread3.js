65f9f852a4c37333125041484364e2489ef6abea
Prem Kumar <prem68265@gmail.com>
2025-01-25 11:44:28 +0530

    Update thread3.js

--- controller/BrandScrap/thread3.js
+++ controller/BrandScrap/thread3.js
@@ -2,131 +2,10 @@
 require('dotenv').config();
 const apikey = process.env.API_KEY
 const BrandUrl = require('../../model/Brand_model/brandurl');
-const cheerio = require('cheerio')
 const Product = require('../../model/Brand_model/products');
 const { ZenRows } = require("zenrows");
-// const Varientupc = require('../../model/Brand_model/varientupc')
+const {generatesku, fetchAndExtractVariable, fetchProductData}= require('../utils')
 
-// -------------save varient wise upc list--------
-// const savevarientlist = async (html) => {
-//     try {
-//         const $ = cheerio.load(html);
-//         let productData = null;
-//         $('script').each((index, element) => {
-//             const scriptContent = $(element).html();
-//             const regex = /window\.product\s*=\s*({[^]*?});/;
-//             const match = scriptContent.match(regex);
-//             if (match) {
-//                 try {
-//                     productData = JSON.parse(match[1]);
-
-//                 } catch (error) {
-//                     console.error("Failed to parse JSON:", error);
-//                 }
-//             }
-//         });
-//         const colorToSize = productData.colorSizeMap.colorToSize
-//         const result = {};
-//         for (const color in colorToSize) {
-//             const values = Object.values(colorToSize[color]);
-//             result[color] = values;
-//         }
-//         for (const i in result) {
-//             let obj = {
-//                 varient: i,
-//                 upc: result[i]
-//             }
-//             let newvarient = new Varientupc(obj);
-//             await newvarient.save();
-//         }
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
-
-// --------start scrapping upc--------
-async function fetchAndExtractVariable(html, variableName) {
-    const $ = cheerio.load(html);
-    let variableValue;
-    $('script').each((index, script) => {
-        const scriptContent = $(script).html();
-        const regex = new RegExp(`${variableName}\\s*=\\s*({[^]*?});`);
-        const match = regex.exec(scriptContent);
-
-        if (match) {
-            try {
-                variableValue = JSON.parse(match[1]);
-            } catch (error) {
-                console.error("Failed to parse JSON:", error);
-            }
-        }
-    });
-    return variableValue;
-}
-
-async function fetchProductData(html) {
-    try {
-        const $ = cheerio.load(html);
-        let productData = null;
-        $('script').each((index, element) => {
-            const scriptContent = $(element).html();
-            const regex = /window\.product\s*=\s*({[^]*?});/;
-            const match = scriptContent.match(regex);
-            if (match) {
-                try {
-                    // Parse the matched JSON-like object
-                    productData = JSON.parse(match[1]);
-
-                } catch (error) {
-                    console.error("Failed to parse JSON:", error);
-                }
-            }
-        });
-
-        if (productData) {
-            return productData;
-        } else {
-            console.log("Product data not found in HTML.");
-            return null;
-        }
-    } catch (error) {
-        console.error("Error fetching HTML:", error);
-        return null;
-    }
-}
-
-const generatesku=(upc,color,size)=>{
-    if(color && size){
-     let a= size.split(' ');
-     a[1]= a[1].slice(0,1)
-     a=a.join('');
-     size=a
-     color= color.replaceAll(' ','-').replaceAll('/','-').toUpperCase();
-     let firstletter= color.charAt(0)
-     color= color.slice(1)
-     var modifiedColor=color
-     if(color.length>12){
-         let v=['A','E','I','O','U'];
-         for (let i of v){
-             modifiedColor  = color.replaceAll(i,'');
-             color= modifiedColor
-         }
-     }
-     if(color.length>12){
-         let arr= color.split('-');
-         for (let i=0; i<arr.length; i++){
-             arr[i]= arr[i].slice(0,3)
-         }
-         color= arr.join('-')
-     }
-   let sku='RC-R1-'+upc+'-'+firstletter+color+'-'+size
-   sku.replace('--','-')
-   sku.replace('--','-')
-   return sku;
-    }else{
-     return null
-    }
- }
 
 const getupc = async (url) => {
     try {
@@ -136,7 +15,6 @@ const getupc = async (url) => {
             js_render: true,
         });
         const html = await request.text();
-        // await savevarientlist(html)
         let clrsize = await fetchProductData(html);
         const size = clrsize.colorSizeMap.colorToSize
         const color = clrsize.colorSizeMap.colors;
@@ -168,19 +46,15 @@ const getupc = async (url) => {
             const onsale = data.sku_on_sale;
             const imgurl = data.sku_image_url
             const coupon = data.product_promotedCoupon[0].cpnDiscount !== undefined ? data.product_promotedCoupon[0].cpnDiscount : null;
-            const offerend = coupon === null ? null : data.product_promotedCoupon[0].endDate.slice(0, 10);
             let products = upc.map((u, index) => ({
                 name: name,
                 upc: u,
                 price: Number(data.product_promotedCoupon[0].cpnDiscount) > 0 && Boolean(onsale[index]) === false ? price[index] * (1 - (coupon / 100)) : price[index],
                 quantity: Number(num[index]),
-                onsale: onsale[index],
                 productid: id[index],
                 color: color_size[id[index]] ? color_size[id[index]].color : null,
                 size: color_size[id[index]] ? color_size[id[index]].size : null,
                 sku:generatesku(u,color_size[id[index]] ? color_size[id[index]].color : null,color_size[id[index]] ? color_size[id[index]].size : null,),
-                discount: onsale[index] ? 0 : Number(coupon),
-                offerend: offerend,
                 url: url,
                 imgurl: imgurl[index]
             }));
@@ -209,11 +83,11 @@ exports.getproduct3 = async (req, res) => {
                 { _id: arrayid },
                 { $pull: { producturl: url } }
             )
+            res.status(200).json({ status: true })
         } else {
+            res.status(200).json({ status: false })
             console.log("UPC data not found or error occurred for:", url);
         }
-        res.status(200).json({ status: true })
-
     } catch (err) {
         console.error("Error:", err);
         res.status(500).json({ status: false, error: 'Failed to fetch product data' });
