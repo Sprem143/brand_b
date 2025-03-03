33a83fe0c7f4d09333b4f0f76389269d897866a6
Prem Kumar <prem68265@gmail.com>
2025-03-03 16:16:09 +0530

    Add files via upload

---
+++ model/Inventory_model/outofstock.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const dataSchema = new mongoose.Schema({
+    'Input UPC': {
+        type: String
+    },
+    'Vendor URL': String,
+    ASIN: String,
+    SKU: String,
+    'Product price': Number,
+    'Amazon link': String,
+    'Image link': String,
+    'Available Quantity': Number,
+    'Product link': String,
+    'Fulfillment': Number,
+    'Amazon Fees%': String,
+    'Shipping Template': String,
+    'Min Profit': String,
+    Date: {
+        type: String,
+        default: () => new Date().toLocaleDateString("en-GB"),  
+    },
+
+
+});
+module.exports = mongoose.model('Outofstock', dataSchema);
