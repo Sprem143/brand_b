38993885c81261ca673c7b5112003a78b3ce4a1d
Prem Kumar <prem68265@gmail.com>
2025-03-04 16:46:48 +0530

    Update db_get.js

--- controller/database_controller/db_get.js
+++ controller/database_controller/db_get.js
@@ -1,9 +1,9 @@
 const InvProduct = require('../../model/Inventory_model/invProduct');
 const AutoFetchData = require('../../model/Inventory_model/autofetchdata')
-const Backup= require('../../model/Inventory_model/backup');
+const Backup = require('../../model/Inventory_model/backup');
 
 
-exports.getrowdata = async(req, res) => {
+exports.getrowdata = async (req, res) => {
     try {
         let rowData = await InvProduct.find();
         res.status(200).send(rowData)
@@ -12,9 +12,9 @@ exports.getrowdata = async(req, res) => {
         res.send(err)
     }
 }
-exports.getdata = async(req, res) => {
+exports.getdata = async (req, res) => {
     try {
-       let resultData = await AutoFetchData.find();
+        let resultData = await AutoFetchData.find();
         res.status(200).send(resultData)
     } catch (err) {
         console.log(err);
@@ -22,16 +22,36 @@ exports.getdata = async(req, res) => {
     }
 }
 
-exports.getbackup = async(req, res) => {
+exports.getbackup = async (req, res) => {
     try {
-       let filename = await Backup.find({}, { name: 1, _id: 0 } );
-        console.log(filename)
-        let lastfile= filename[filename.length-1].name
-        let data= await Backup.findOne({name:lastfile})
-     let resultData=[data]
-        res.status(200).send(resultData)
+        let list = await Backup.find({}, { name: 1, length:1, _id: 0 });
+        let data= await Backup.findOne({name: list[list.length-1].name})
+        res.status(200).json({ status: true, list: list, data: data.data })
     } catch (err) {
         console.log(err);
-        res.send(err)
+         res.status(500).json({status:false, msg:err})
+    }
+}
+
+exports.getonebackup=async(req,res)=>{
+    try{
+    const {name}= req.body;
+     let data=  await Backup.findOne({name:name});
+     res.status(200).json({status:true, data:data})
+    }catch(err){
+        console.log(err)
+        res.staus(500).json({status:false, msg:err})
+    }
+}
+
+exports.downloadbackup=async(req,res)=>{
+    try{
+const {name}= req.body;
+let data= await Backup.findOne({name:name})
+res.status(200).json({data:data.data})
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({status:false, msg:err})
     }
 }
