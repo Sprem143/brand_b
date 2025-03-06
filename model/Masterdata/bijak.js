1b40f041de2163df48e5c8974c63629cfe3bc1ed
Prem Kumar <prem68265@gmail.com>
2025-03-06 18:35:42 +0530

    Add files via upload

---
+++ model/Masterdata/bijak.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const dataSchema = new mongoose.Schema({
+    ASIN: String,
+    UPC:String,
+    SKU: String,
+    Date: {
+        type: String,
+        default: () => new Date().toLocaleDateString("en-GB"),  
+    },
+});
+module.exports = mongoose.model('Bijak', dataSchema);
