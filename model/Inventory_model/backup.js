cb0c6da6d9d72cff3740a5edf43c9998b5d43bc5
Prem Kumar <prem68265@gmail.com>
2025-03-07 18:29:34 +0530

    Update backup.js

--- model/Inventory_model/backup.js
+++ model/Inventory_model/backup.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const urlSchema = new mongoose.Schema({
     name: {
         type: String,
-        default: () => new Date().toLocaleString(),  
+        default: () => new Date().toLocaleString(),
     },
+    account:String,
     data: {
         type: [Object],
-        required: true,  
+        required: true,
     },
-    length:Number
-}, {
-    timestamps: true,  
+    length: Number
+}, 
+{
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Backup', urlSchema);
