a38ee3b6479f03f864a432b9f2b81949197920ad
Prem Kumar <prem68265@gmail.com>
2025-03-11 16:28:53 +0530

    Update invProduct.js

--- model/Inventory_model/invProduct.js
+++ model/Inventory_model/invProduct.js
@@ -4,18 +4,17 @@ const dataSchema = new mongoose.Schema({
     'Input UPC': {
         type: String
     },
-    'Vendor URL': String,
     ASIN: String,
     SKU: String,
     'Product price': Number,
-    'Amazon link': String,
-    'Image link': String,
     'Available Quantity': Number,
     'Product link': String,
     'Fulfillment': Number,
     'Amazon Fees%': String,
     'Shipping Template': String,
-    'Min Profit': String
+    'Min Profit': String,
+    'Brand Name':String,
+    'Amazon Title':String
 
 });
 module.exports = mongoose.model('InvProduct', dataSchema);
