9773260a89f30e23117522b9810ff53ccc01c0a5
Prem Kumar <prem68265@gmail.com>
2025-03-11 16:25:13 +0530

    Update om.js

--- model/Masterdata/om.js
+++ model/Masterdata/om.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
 
 const dataSchema = new mongoose.Schema({
+    seller:String,
     ASIN: String,
-    UPC:String,
+    'Input UPC':String,
     SKU: String,
+    'Brand Name':String,
+    'Amazon Title':String,
+    'Product link': String,
+    'Current Price':Number,
+    'Current Quantity':String,
+    'Fulfillment':Number,
+    'Amazon Fees%':String,
+    'Shipping Template':Number,
+    'Min Profit':Number,
+    'PriceRange':Array,
     Date: {
         type: String,
         default: () => new Date().toLocaleDateString("en-GB"),  
