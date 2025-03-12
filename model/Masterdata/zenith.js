d0f70c650e6272c669180b71cc9c7c6ca5674361
Prem Kumar <prem68265@gmail.com>
2025-03-12 12:10:54 +0530

    Update zenith.js

--- model/Masterdata/zenith.js
+++ model/Masterdata/zenith.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose')
 
 const dataSchema = new mongoose.Schema({
-    seller:String,
     ASIN: String,
     'Input UPC':String,
     SKU: String,
     'Brand Name':String,
     'Amazon Title':String,
     'Product link': String,
-    'Current Price':Number,
-    'Current Quantity':String,
     'Fulfillment':Number,
-    'Amazon Fees%':String,
     'Shipping Template':Number,
-    'Min Profit':Number,
-    'PriceRange':Array,
+    img:String,
     Date: {
         type: String,
         default: () => new Date().toLocaleDateString("en-GB"),  
